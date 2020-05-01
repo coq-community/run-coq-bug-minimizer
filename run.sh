a65014ebefae26afa5c384084dc8c7a69b001ffd
Jason Gross <jgross@mit.edu>
2020-05-01 15:47:36 -0400

    Try out minimization

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+
+opam install -y coq-equations dune
+git clone https://github.com/Mbodin/coq-prelude.git --branch=bug
+cd coq-prelude
+dune build @all || true
+yes "y" | find-bug.py theories/Control/List.v bug.v -f _CoqProject -l - bug.log
