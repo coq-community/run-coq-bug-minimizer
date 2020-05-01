371ef0edd098bb02ffc42392dd364ed26d02f4e0
Jason Gross <jgross@mit.edu>
2020-05-01 18:33:08 -0400

    Build in parent dir

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ eval $(opam env)
 git clone https://github.com/Mbodin/coq-prelude.git --branch=bug
 cd coq-prelude
 dune build @all || true
-yes "y" | find-bug.py theories/Control/List.v bug.v -f _CoqProject -l - bug.log
+yes "y" | find-bug.py theories/Control/List.v ../bug.v -f _CoqProject -l - ../bug.log
