b515d6177f9cf6630956a4e8fd4c9a44e2ea7e3c
Jason Gross <jgross@mit.edu>
2020-05-02 19:22:04 -0400

    Run the minimizer a second time

--- run.sh
+++ run.sh
@@ -16,5 +16,7 @@ eval $(opam env)
 git clone https://github.com/Mbodin/coq-prelude.git --branch=bug
 cd coq-prelude
 dune build @all || true
-(yes "y" || true) | find-bug.py _build/default/theories/Control/List.v _build/default/theories/bug.v _build/default/theories/tmp.v -f _CoqProject -Q theories Fake -l - ../bug.log && RC=0
+(yes "y" || true) | find-bug.py _build/default/theories/Control/List.v _build/default/theories/bug.v _build/default/theories/tmp.v -f _CoqProject -Q theories Fake -l - ../bug.log
+cp -f _build/default/theories/bug.v _build/default/theories/bug0.v
+(yes "y" || true) | find-bug.py _build/default/theories/bug{0,}.v _build/default/theories/tmp.v -f _CoqProject -Q theories Fake -l - ../bug2.log && RC=0
 cleanup
