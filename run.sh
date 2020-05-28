0d00b64fa07773ba2b7ca980d2b3232bdc401bdb
Jason Gross <jasongross9@gmail.com>
2020-05-28 16:19:43 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 RC=1
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 function cleanup() {
-    cp -f _build/default/theories/bug.v ../bug.v
+    cp -f "$DIR/oak-hardware/cava/cava/Cava/bug.v" "$DIR/bug.v"
     exit $RC
 }
 
@@ -19,7 +21,6 @@ git checkout 38971a7d0f8aa04b6fa4e21d1dfda3990ecf2c66
 cd cava/cava
 
 make coq || true
-(yes "y" || true) | find-bug.py ./Cava/Arrow/Instances/Netlist.v Cava/bug.v Cava/tmp.v -f _CoqProject -l - ../../../bug.log
-cp -f Cava/bug.v ../../../bug.v
+(yes "y" || true) | find-bug.py ./Cava/Arrow/Instances/Netlist.v Cava/bug.v Cava/tmp.v -f _CoqProject -l - "$DIR/bug.log" && RC=0
 #(yes "y" || true) | find-bug.py _build/default/theories/bug{0,}.v _build/default/theories/tmp.v -f _CoqProject -Q theories Fake -l - ../bug2.log && RC=0
 cleanup
