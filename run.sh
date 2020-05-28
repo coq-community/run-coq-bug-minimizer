27f224ff3d0c2c79cf764d636ccb47c264ec0bd2
Jason Gross <jasongross9@gmail.com>
2020-05-28 16:17:25 -0400

    Minimize another bug
    https://github.com/coq/coq/issues/12418

--- run.sh
+++ run.sh
@@ -11,12 +11,15 @@ trap cleanup SIGINT SIGKILL EXIT
 
 set -ex
 
-opam install -y coq-equations dune
+# opam install -y coq-equations dune
 eval $(opam env)
-git clone https://github.com/Mbodin/coq-prelude.git --branch=bug
-cd coq-prelude
-dune build @all || true
-(yes "y" || true) | find-bug.py _build/default/theories/Control/List.v _build/default/theories/bug.v _build/default/theories/tmp.v -f _CoqProject -Q theories Fake -l - ../bug.log
-cp -f _build/default/theories/bug.v _build/default/theories/bug0.v
-(yes "y" || true) | find-bug.py _build/default/theories/bug{0,}.v _build/default/theories/tmp.v -f _CoqProject -Q theories Fake -l - ../bug2.log && RC=0
+git clone https://github.com/satnam6502/oak-hardware
+cd oak-hardware
+git checkout 38971a7d0f8aa04b6fa4e21d1dfda3990ecf2c66
+cd cava/cava
+
+make coq || true
+(yes "y" || true) | find-bug.py ./Cava/Arrow/Instances/Netlist.v Cava/bug.v Cava/tmp.v -f _CoqProject -l - ../../../bug.log
+cp -f Cava/bug.v ../../../bug.v
+#(yes "y" || true) | find-bug.py _build/default/theories/bug{0,}.v _build/default/theories/tmp.v -f _CoqProject -Q theories Fake -l - ../bug2.log && RC=0
 cleanup
