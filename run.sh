d1ff10735d05864c0c30c6754aff6aa713d7d1a2
Jason Gross <jgross@mit.edu>
2020-05-31 18:56:23 -0400

    Only look for one error message

--- run.sh
+++ run.sh
@@ -17,9 +17,9 @@ source "$DIR/coqbot.sh" 2>"$DIR/log"
 
 set -e
 
-cat "$DIR/log"
+cat "$DIR/build.log"
 
-FILE="$(grep '^File "[^"]*", line [0-9]*, characters [0-9-]*:' "$DIR/log" | grep -o '^File "[^"]*' | sed 's/^File "//g')"
+FILE="$(tac "$DIR/build.log" | grep --max-count=1 -A 1 '^Error' | grep '^File "[^"]*", line [0-9]*, characters [0-9-]*:' | grep -o '^File "[^"]*' | sed 's/^File "//g')"
 
 (yes "y" || true) | find-bug.py "$FILE" bug_01.v tmp.v --inline-user-contrib -l - "$DIR/bug.log" && RC=0
 cleanup
