9584d46f3f12bc5df2d4436afc5b2a1556a4fba1
Jason Gross <jgross@mit.edu>
2020-07-15 17:32:42 -0400

    Don't run cleanup twice

--- run.sh
+++ run.sh
@@ -28,4 +28,3 @@ cat "$DIR/build.log"
 FILE="$(tac "$DIR/build.log" | grep --max-count=1 -A 1 '^Error' | grep '^File "[^"]*", line [0-9]*, characters [0-9-]*:' | grep -o '^File "[^"]*' | sed 's/^File "//g')"
 
 (yes "y" || true) | find-bug.py "$FILE" bug_01.v tmp.v --inline-user-contrib -l - "$DIR/bug.log" && RC=0
-cleanup
