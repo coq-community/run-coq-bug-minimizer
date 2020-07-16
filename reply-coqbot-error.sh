b12581794261ec64b62419c2f060b76d1cbe8bb6
Julien Coolen <jtcoolen@pm.me>
2020-07-16 21:04:38 +0200

    Missing new lines in reply messages.

--- reply-coqbot-error.sh
+++ reply-coqbot-error.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 id="$1"
 comment_contents="Error: Could not minimize file $2"
-comment_contents+="${nl}<details><summary>error log</summary>${nl}${nl}${start_code}$(cat "$3")${end_code}</details>"
+comment_contents+="${nl}<details><summary>error log</summary>${nl}${nl}${start_code}$(cat "$3")${end_code}${nl}</details>"
 
 if [ ! -z "${COQBOT_URL}" ]; then
     curl -X POST --data-binary "${id}${nl}${comment_contents}" "${COQBOT_URL}"
