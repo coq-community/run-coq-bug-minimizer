592c5ba6e94228640ed1946b7cd92ccf6eecac3c
Julien Coolen <jtcoolen@pm.me>
2020-07-16 21:04:38 +0200

    Missing new lines in reply messages.

--- reply-coqbot.sh
+++ reply-coqbot.sh
@@ -6,9 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 id="$1"
 comment_contents="Minimized File $2"
-comment_contents+="${nl}${nl}<details><summary>Minimized Coq File</summary>${nl}${nl}${start_coq_code}$(cat "$3")${end_code}</details>"
-comment_contents+="${nl}${nl}<details><summary>Build Log</summary>${nl}${nl}${start_code}$(cat "$4")${end_code}</details>"
-comment_contents+="${nl}${nl}<details><summary>Minimization Log</summary>${nl}${nl}${start_code}$(cat "$5")${end_code}</details>"
+comment_contents+="${nl}${nl}<details><summary>Minimized Coq File</summary>${nl}${nl}${start_coq_code}$(cat "$3")${end_code}${nl}</details>"
+comment_contents+="${nl}${nl}<details><summary>Build Log</summary>${nl}${nl}${start_code}$(cat "$4")${end_code}${nl}</details>"
+comment_contents+="${nl}${nl}<details><summary>Minimization Log</summary>${nl}${nl}${start_code}$(cat "$5")${end_code}${nl}</details>"
 
 if [ ! -z "${COQBOT_URL}" ]; then
     curl -X POST --data-binary "${id}${nl}${comment_contents}" "${COQBOT_URL}"
