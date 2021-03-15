78e1981ae308317799d0e471bd321ab2a9f711cf
Jason Gross <jgross@mit.edu>
2021-03-15 13:18:22 -0400

    Avoid /usr/bin/curl: Argument list too long
    cf https://github.com/coq-community/run-coq-bug-minimizer/runs/2114450949#step:4:1517

--- reply-coqbot-error.sh
+++ reply-coqbot-error.sh
@@ -10,8 +10,15 @@ comment_contents="Error: Could not minimize file $2"
 comment_contents+="${nl}<details><summary>error log</summary>${nl}${nl}${start_code}$(cat "$3")${end_code}${nl}</details>"
 comment_contents+="${nl}${nl}$(cat "$DIR/feedback.md")"
 
+file="$(mktemp)"
+cat > "$file" <<EOF
+${id}${nl}${comment_contents}
+EOF
+
 if [ ! -z "${COQBOT_URL}" ]; then
-    curl -X POST --data-binary "${id}${nl}${comment_contents}" "${COQBOT_URL}"
+    curl -X POST --data-binary "@${file}"
 else
-    echo curl -X POST --data-binary "${id}${nl}${comment_contents}"
+    echo curl -X POST --data-binary "@${file}"
+    echo cat "$file"
+    cat "$file"
 fi
