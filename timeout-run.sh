cc54a557108d98756436ef6102c019c602029524
Jason Gross <jgross@mit.edu>
2021-03-19 09:01:19 -0400

    use a timeout to ensure that we respond to coqbot

--- timeout-run.sh
+++ timeout-run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-timeout 5 "$DIR/run.sh"
+# use 5h15m as the timeout; the timeout for this stage is 5h30m, so we leave a generous allotment for docker
+timeout 18900 "$DIR/run.sh"
