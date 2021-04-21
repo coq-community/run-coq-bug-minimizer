e17697cd53de06fbfa65c6e514527621ceb653d6
coqbot <coqbot@users.noreply.github.com>
2021-04-21 00:23:31 +0000

    Added user script in coqbot.sh

--- coqbot.sh
+++ coqbot.sh
@@ -1,17 +1,6 @@
-opam update -y
-opam install -y coq-ext-lib
-eval $(opam env)
 
-sudo apt-get update -y
-sudo apt-get install -y wget
+git clone https://gitlab.mpi-sws.org/iris/iris.git && cd iris && git checkout 59d18188f9db844a75f5e37dc7cbc6c39ecbb24f
+opam repo add iris-dev https://gitlab.mpi-sws.org/iris/opam.git
+make builddep OPAMFLAGS=-y
+make -j2 tests/one_shot.vo
 
-mkdir temp
-cd temp
-wget https://github.com/coq/coq/files/4698509/bug.v.zip
-unzip bug.v.zip
-coqc -q bug.v
-#git clone https://github.com/satnam6502/oak-hardware
-#cd oak-hardware
-#git checkout 38971a7d0f8aa04b6fa4e21d1dfda3990ecf2c66
-#cd cava/cava
-#make coq
