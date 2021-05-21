59c750b9fb21161cdb52d3d21137f0ad19b73de7
Jason Gross <jgross@mit.edu>
2021-05-21 12:42:40 -0400

    Mention ci target

--- run.sh
+++ run.sh
@@ -9,12 +9,16 @@ source "$DIR/coqbot-config.sh"
 function cleanup() {
     cp "${BUG_FILE}" "${FINAL_BUG_FILE}" || RC=$?
     STAMP="$(cat "$DIR/coqbot-request-stamp")"
+    EXTRA_DESCRIPTION=""
+    if [ ! -z "${CI_TARGET}" ]; then
+        EXTRA_DESCRIPTION=" (from ${CI_TARGET})"
+    fi
     if [ -f "${FINAL_BUG_FILE}" ]; then
         touch "${TMP_FILE}" "${BUILD_LOG}" "${BUG_LOG}"
-        bash "$DIR/reply-coqbot.sh" "$STAMP" "$FILE" "${FINAL_BUG_FILE}" "${TMP_FILE}" "${BUILD_LOG}" "${BUG_LOG}"
+        bash "$DIR/reply-coqbot.sh" "$STAMP" "${FILE}${EXTRA_DESCRIPTION}" "${FINAL_BUG_FILE}" "${TMP_FILE}" "${BUILD_LOG}" "${BUG_LOG}"
     else
         touch "${BUILD_LOG}" "${BUG_LOG}"
-        bash "$DIR/reply-coqbot-error.sh" "$STAMP" "$FILE" "${BUILD_LOG}" "${BUG_LOG}"
+        bash "$DIR/reply-coqbot-error.sh" "$STAMP" "${FILE}${EXTRA_DESCRIPTION}" "${BUILD_LOG}" "${BUG_LOG}"
     fi
     exit $RC
 }
