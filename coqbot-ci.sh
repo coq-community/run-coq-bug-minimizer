c5073cad137b5b2ecfe3ec974edf7c18a9e57138
Jason Gross <jgross@mit.edu>
2021-07-01 19:12:17 -0400

    Use mv rather than ln -s
    This should fix an issue with ci-tlc where .v.d files were generated
    with absolute paths on Coq's CI and then pointed to locations that
    `make` could not see on GH Actions.  By making paths align more when
    building the files, we can avoid this issue.
    
    Should fix things like https://github.com/coq/coq/pull/13107#issuecomment-870664233

--- coqbot-ci.sh
+++ coqbot-ci.sh
@@ -66,18 +66,19 @@ sudo mkdir -p "${COQ_CI_BASE_BUILD_DIR}"
 sudo rm -rf "${COQ_CI_BASE_BUILD_DIR}"
 
 echo "::group::make ${CI_TARGET} (passing)"
-pushd "${CI_BASE_BUILD_DIR}"/coq-passing
-sudo ln -s "$(pwd)" "${COQ_CI_BASE_BUILD_DIR}"
+mv "${CI_BASE_BUILD_DIR}"/coq-passing "${COQ_CI_BASE_BUILD_DIR}"
+pushd "${COQ_CI_BASE_BUILD_DIR}"
 make -f Makefile.ci GITLAB_CI=1 ${CI_TARGET}
 popd
+mv "${COQ_CI_BASE_BUILD_DIR}" "${CI_BASE_BUILD_DIR}"/coq-passing
 echo '::endgroup::'
 
 echo "::group::make ${CI_TARGET} (failing)"
-pushd "${CI_BASE_BUILD_DIR}"/coq-failing
-sudo rm "${COQ_CI_BASE_BUILD_DIR}"
-sudo ln -s "$(pwd)" "${COQ_CI_BASE_BUILD_DIR}"
+mv "${CI_BASE_BUILD_DIR}"/coq-failing "${COQ_CI_BASE_BUILD_DIR}"
+pushd "${COQ_CI_BASE_BUILD_DIR}"
 make -f Makefile.ci GITLAB_CI=1 ${CI_TARGET} 2>&1 || true
 popd
+mv "${COQ_CI_BASE_BUILD_DIR}" "${CI_BASE_BUILD_DIR}"/coq-failing
 echo '::endgroup::'
 
 set -x
