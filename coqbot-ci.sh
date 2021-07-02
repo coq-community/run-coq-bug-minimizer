54af5ae4870afe1bfc81fa7e543c84f4ec8aa438
Jason Gross <jgross@mit.edu>
2021-07-02 16:23:01 -0400

    Add more debug information

--- coqbot-ci.sh
+++ coqbot-ci.sh
@@ -13,6 +13,8 @@ echo "::group::opam switch '$COMPILER'"
 echo "::warning::Using opam switch '$COMPILER'"
 opam switch "$COMPILER"
 eval $(opam env)
+echo "::warning::which ocamlfind: '$(which ocamlfind)'"
+echo "::warning::ocamlfind ocamlopt: '$(ocamlfind ocamlopt)'"
 echo '::endgroup::'
 
 mkdir -p "${CI_BASE_BUILD_DIR}"
@@ -53,6 +55,7 @@ echo '::endgroup::'
 echo '::group::wrap binaries'
 for dir in "${CI_BASE_BUILD_DIR}"/coq-{failing,passing}/_install_ci/bin; do
     pushd "$dir" >/dev/null
+    echo "::warning::$(pwd)/coqc -config: $(./coqc -config | xargs printf "%s%%0A")"
     for i in $(ls); do
         wrap_file "$i"
     done
