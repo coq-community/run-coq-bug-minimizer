182382c61e98a0386cfa07ae255c4e40b53bc7be
Jason Gross <jgross@mit.edu>
2021-12-30 10:40:57 -0800

    Also upload a metadata file
    This should contain enough information so that coqbot doesn't need to
    listen to the curl request to generate the comment.

--- run.sh
+++ run.sh
@@ -17,16 +17,21 @@ function cleanup() {
     cp -a /tmp "${FINAL_TMP_FOLDER}" || true
     chmod -R a+rw "${FINAL_TMP_FOLDER}" || true
     find "${FINAL_TMP_FOLDER}" | xargs ls -la
+    touch "${METADATA_FILE}"
     STAMP="$(cat "$DIR/coqbot-request-stamp")"
+    echo "STAMP=${STAMP}" >> "${METADATA_FILE}"
     touch "$DIR/filename"
     FILE="$(cat "$DIR/filename")"
+    echo "FILE=${FILE}" >> "${METADATA_FILE}"
     EXTRA_DESCRIPTION=""
+    echo "CI_TARGET=${CI_TARGET}" >> "${METADATA_FILE}"
     if [ ! -z "${CI_TARGET}" ]; then
         EXTRA_DESCRIPTION=" (from ${CI_TARGET})"
     fi
     if [ -f "${TIMEDOUT_STAMP_FILE}" ]; then # timeout!
         EXTRA_DESCRIPTION="${EXTRA_DESCRIPTION} (interrupted by timeout"
         if [ ! -z "${COQBOT_RESUME_MINIMIZATION_URL}" ]; then
+            echo "COQBOT_RESUME_MINIMIZATION_URL=${COQBOT_RESUME_MINIMIZATION_URL}" >> "${METADATA_FILE}"
             EXTRA_DESCRIPTION="${EXTRA_DESCRIPTION}, being automatically continued"
         fi
         EXTRA_DESCRIPTION="${EXTRA_DESCRIPTION})"
@@ -34,12 +39,15 @@ function cleanup() {
     if [ -f "${FINAL_BUG_FILE}" ]; then
         touch "${BUILD_LOG}" "${BUG_LOG}"
         if [ -f "${TIMEDOUT_STAMP_FILE}" ]; then # timeout!
+            echo "TIMEDOUT=1" >> "${METADATA_FILE}"
+            echo "RESUMPTION_ARGS=${RESUMPTION_ARGS}" >> "${METADATA_FILE}"
             bash "$DIR/reply-coqbot-timeout.sh" "$STAMP" "${FILE}${EXTRA_DESCRIPTION}" "${FINAL_BUG_FILE}" "${FINAL_TMP_FILE}" "${BUILD_LOG}" "${BUG_LOG}"
         else
             bash "$DIR/reply-coqbot.sh" "$STAMP" "${FILE}${EXTRA_DESCRIPTION}" "${FINAL_BUG_FILE}" "${FINAL_TMP_FILE}" "${BUILD_LOG}" "${BUG_LOG}"
         fi
     else
         touch "${BUILD_LOG}" "${BUG_LOG}"
+        echo "ERROR=1" >> "${METADATA_FILE}"
         bash "$DIR/reply-coqbot-error.sh" "$STAMP" "${FILE}${EXTRA_DESCRIPTION}" "${BUILD_LOG}" "${BUG_LOG}"
     fi
     echo '::endgroup::'
