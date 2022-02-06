08bdabf3f5850a766c121ebf51746d2643b4794c
coqbot <coqbot@users.noreply.github.com>
2022-02-06 00:43:53 +0000

    Added user script in coqbot.sh

--- coqbot.sh
+++ coqbot.sh
@@ -1,14 +1,6 @@
-opam update -y
-opam install -y coq-ext-lib
+opam install -y coq.8.7.2
 eval $(opam env)
-
-mkdir temp
-cd temp
-wget https://github.com/coq/coq/files/4698509/bug.v.zip
-unzip bug.v.zip
-coqc -q bug.v
-#git clone https://github.com/satnam6502/oak-hardware
-#cd oak-hardware
-#git checkout 38971a7d0f8aa04b6fa4e21d1dfda3990ecf2c66
-#cd cava/cava
-#make coq
+git clone git@github.com:alizter/VST.git
+cd VST
+git checkout test-6984
+make veric/SeparationLogic.vo
