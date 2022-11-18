9251d60cc0b30bd4b449c1e1cdfbc8d3414a93a0
coqbot <coqbot@users.noreply.github.com>
2022-11-18 17:39:31 +0000

    Added user script for MDU6SXNzdWU2NjAzOTg1NTA= in coqbot.sh for JasonGross

--- coqbot.sh
+++ coqbot.sh
@@ -1,14 +1,2 @@
-opam update -y
-opam install -y coq-ext-lib
-eval $(opam env)
-
-mkdir temp
-cd temp
-wget https://github.com/coq/coq/files/4698509/bug.v.zip
-unzip bug.v.zip
-coqc -q bug.v
-#git clone https://github.com/satnam6502/oak-hardware
-#cd oak-hardware
-#git checkout 38971a7d0f8aa04b6fa4e21d1dfda3990ecf2c66
-#cd cava/cava
-#make coq
+true | coqtop
+opam install -y coq-fiat-crypto.dev
