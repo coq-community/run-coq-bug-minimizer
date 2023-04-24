b09964640b6aadc17366fad1be5dca71f0c80ec8
coqbot <coqbot@users.noreply.github.com>
2023-04-24 22:57:46 +0000

    Added user script in coqbot.sh for JasonGross in coq/coq
    Comment Thread ID: I_kwDOABUDh85kQ6RG

--- coqbot.sh
+++ coqbot.sh
@@ -1,14 +1,5 @@
-opam update -y
-opam install -y coq-ext-lib
-eval $(opam env)
-
-mkdir temp
-cd temp
-wget https://github.com/coq/coq/files/4698509/bug.v.zip
-unzip bug.v.zip
-coqc -q bug.v
-#git clone https://github.com/satnam6502/oak-hardware
-#cd oak-hardware
-#git checkout 38971a7d0f8aa04b6fa4e21d1dfda3990ecf2c66
-#cd cava/cava
-#make coq
+opam install -y coq-equations
+git clone https://github.com/JasonGross/metacoq.git --branch=zzz-bug-slow-qed
+cd metacoq
+./configure.sh local
+make quotation TIMED=1 -j2
