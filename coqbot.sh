70d1ff10d30c7f006db1a194b66dd2dc31d01e0e
coqbot <coqbot@users.noreply.github.com>
2023-09-23 20:26:17 +0000

    Added user script in coqbot.sh for SkySkimmer in coq/coq
    Comment Thread ID: I_kwDOABUDh85xKks9

--- coqbot.sh
+++ coqbot.sh
@@ -1,14 +1,5 @@
+git clone https://github.com/skyskimmer/CertiGraph.git --branch=refine-bug
+cd CertiGraph
 opam update -y
-opam install -y coq-ext-lib
-eval $(opam env)
-
-mkdir temp
-cd temp
-wget https://github.com/coq/coq/files/4698509/bug.v.zip
-unzip bug.v.zip
-coqc -q bug.v
-#git clone https://github.com/satnam6502/oak-hardware
-#cd oak-hardware
-#git checkout 38971a7d0f8aa04b6fa4e21d1dfda3990ecf2c66
-#cd cava/cava
-#make coq
+opam install -y coq-vst.2.12
+make -j CertiGC/refine_bug.vo
