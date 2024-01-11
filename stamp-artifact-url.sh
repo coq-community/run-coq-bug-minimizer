6907d2761d7d9c9234aa9aa78e54fd2732411257
Jason Gross <jgross@mit.edu>
2024-01-11 17:22:05 +0000

    Upload tmp.log (will eventually exist) and only create url files if the steps succeeded

---
+++ stamp-artifact-url.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+function usage() {
+    printf 'USAGE: %q FILE_NAME ARTIFACT_ID OUTCOME\n' "$0"
+    # outcome should be success, failure, cancelled, or skipped, cf https://stackoverflow.com/a/70549615/377022
+    exit 1
+}
+
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    usage
+fi
+
+filename="$1"
+artifact_id="$2"
+outcome="$3"
+if [ "${outcome}" == "success" ]; then
+    printf '%s' "${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/actions/artifacts/${artifact_id}/zip" | tee "${filename}.api.url"
+    echo
+    printf '%s' "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/artifacts/${artifact_id}" | tee "${filename}.url"
+    echo
+fi
