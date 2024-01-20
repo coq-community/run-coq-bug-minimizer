04bc274321c9053039839dd683f362c17d94bffa
coqbot <coqbot@users.noreply.github.com>
2024-01-20 06:30:41 +0000

    Added user script in coqbot.sh for JasonGross in coq/coq
    Comment Thread ID: I_kwDOABUDh858kjSA

--- coqbot.sh
+++ coqbot.sh
@@ -1,14 +1,4 @@
-opam update -y
-opam install -y coq-ext-lib
-eval $(opam env)
-
-mkdir temp
-cd temp
-wget https://github.com/coq/coq/files/4698509/bug.v.zip
-unzip bug.v.zip
-coqc -q bug.v
-#git clone https://github.com/satnam6502/oak-hardware
-#cd oak-hardware
-#git checkout 38971a7d0f8aa04b6fa4e21d1dfda3990ecf2c66
-#cd cava/cava
-#make coq
+git clone https://github.com/JasonGross/coq-ext-lib.git --branch=cumul
+cd coq-ext-lib
+opam pin add -y .
+opam install -y coq-quickchick
